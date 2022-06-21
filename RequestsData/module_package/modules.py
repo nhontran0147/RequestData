b918d069318a7098021b1288d9479955bcea2970
unknown <nhontran801@gmail.com>
2022-06-22 02:45:24 +0700

    finish

--- RequestsData/module_package/modules.py
+++ RequestsData/module_package/modules.py
@@ -78,39 +78,49 @@ def show_data(web_url):
     table.heading("post_point", text="Point", anchor=CENTER)
     table.heading("post_creator", text="Creator", anchor=CENTER)
     table.heading("post_comments", text="Comments", anchor=CENTER)
+    page_text = StringVar()
+    page_text.set("Page: " + str(pos_page[0]))
+    lab_num_page = tk.Label(win, textvariable=page_text).place(x=250, y=430)
 
-    for i in range(len(list_post)):
-        table.insert(parent='', index='end', iid=i + 1, text='',
-                     values=(
-                         str(i), list_post[i].get_title(), str(list_post[i].get_point()), list_post[i].get_creator(),
-                         (list_post[i].get_comments())))
+    def refill_table(list_post_):
+        page_text.set("Page: " + str(pos_page[0]))
+        for i in range(len(list_post_)):
+            table.insert(parent='', index='end', iid=i, text='',
+                         values=(
+                             str((30*(pos_page[0]-1)) + i + 1), list_post_[i].get_title(), str(list_post_[i].get_point()),
+                             list_post_[i].get_creator(),
+                             (list_post_[i].get_comments())))
 
     def show_next_table():
         res_page_num(pos_page, True)
         table.delete(*table.get_children())  # Xoa het du lieu cua table
         list_post_next = requests_data(web_url_change + str(pos_page[0]))
-        for j in range(len(list_post_next)):
-            table.insert(parent='', index='end', iid=j + 1, text='',
-                         values=(
-                             str(j), list_post_next[j].get_title(), str(list_post_next[j].get_point()),
-                             list_post_next[j].get_creator(),
-                             (list_post_next[j].get_comments())))
+        refill_table(list_post_next)
+        # for j in range(len(list_post_next)):
+        #     table.insert(parent='', index='end', iid=j, text='',
+        #                  values=(
+        #                      str(j), list_post_next[j].get_title(), str(list_post_next[j].get_point()),
+        #                      list_post_next[j].get_creator(),
+        #                      (list_post_next[j].get_comments())))
 
     def show_previous_table():
         if pos_page[0] != 1:
             res_page_num(pos_page, False)
             table.delete(*table.get_children())  # Xoa het du lieu cua table
             list_post_pre = requests_data(web_url_change + str(pos_page[0]))
-            for j in range(len(list_post_pre)):
-                table.insert(parent='', index='end', iid=j + 1, text='',
-                             values=(
-                                 str(j), list_post_pre[j].get_title(), str(list_post_pre[j].get_point()),
-                                 list_post_pre[j].get_creator(),
-                                 (list_post_pre[j].get_comments())))
+            refill_table(list_post_pre)
+            # for j in range(len(list_post_pre)):
+            #     table.insert(parent='', index='end', iid=j, text='',
+            #                  values=(
+            #                      str(j), list_post_pre[j].get_title(), str(list_post_pre[j].get_point()),
+            #                      list_post_pre[j].get_creator(),
+            #                      (list_post_pre[j].get_comments())))
 
     butt_next = tk.Button(win, text="Next page", width=40, command=show_next_table)
     butt_next.place(x=600, y=430)
     butt_back = tk.Button(win, text="Previous page", width=40, command=show_previous_table)
     butt_back.place(x=300, y=430)
+
+    refill_table(list_post)
     table.pack()
     win.mainloop()
